6fa6f85da5f527e024be3468a914ae61d165ac27
msrocka <michael.srocka@gmail.com>
2017-04-26 14:27:08 +0200

    added build clean; default data for build

---
+++ build/clean.bat
@@ -0,0 +1,5 @@
+@echo off
+
+rmdir dist /s /q
+rmdir win32.win32.x86_64 /s /q
+rmdir macosx.cocoa.x86_64 /s /q
--- build/make.bat
+++ build/make.bat
@@ -1,12 +1,13 @@
 @echo off
 
-SET vdate=v2.0_2017_04_24
+SET vdate=v2.0_2017_04_26
 
 rmdir dist /s /q
 mkdir dist
 
 REM Copy the Java Runtime
 robocopy jre\win64 win32.win32.x86_64\epd-editor\jre /e
+robocopy default_data win32.win32.x86_64\epd-editor\data /e
 
 REM Create the zip packages
 cd win32.win32.x86_64

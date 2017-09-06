f5d67bec05aa9d63af5e85b4274c079fa49e9e36
msrocka <michael.srocka@gmail.com>
2017-09-06 10:11:16 +0200

    updated build scripts

--- build/clean.bat
+++ build/clean.bat
@@ -1,5 +1,16 @@
 @echo off
 
-rmdir dist /s /q
-rmdir win32.win32.x86_64 /s /q
-rmdir macosx.cocoa.x86_64 /s /q
+if exist dist (
+    echo delete folder dist
+    rmdir dist /s /q
+)
+
+if exist win32.win32.x86_64 (
+    echo delete folder win32.win32.x86_64
+    rmdir win32.win32.x86_64 /s /q
+)
+
+if exist macosx.cocoa.x86_64 (
+    echo delete folder macosx.cocoa.x86_64
+    rmdir macosx.cocoa.x86_64 /s /q
+)
--- build/make.bat
+++ build/make.bat
@@ -1,8 +1,22 @@
 @echo off
 
-SET vdate=v2.0_2017_07_26
+SET version=2.0
 
-rmdir dist /s /q
+REM see https://stackoverflow.com/questions/19131029/how-to-get-date-in-bat-file
+FOR /f "tokens=2 delims==" %%a IN ('wmic OS Get localdatetime /value') DO SET "dt=%%a"
+set "YYYY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+
+SET vdate="%version%_%YYYY%_%MM%_%DD%"
+
+IF NOT EXIST win32.win32.x86_64 (
+	ECHO ERROR: Run the PDE - Export first
+	GOTO DONE 
+)
+
+
+if exist dist rmdir dist /s /q
 mkdir dist
 
 REM Copy the Java Runtime
@@ -17,3 +31,5 @@ cd ..
 REM cd macosx.cocoa.x86_64
 REM ..\7za a ..\dist\epd-editor_%vdate%_macosx.zip epd-editor
 REM cd ..
+
+:DONE

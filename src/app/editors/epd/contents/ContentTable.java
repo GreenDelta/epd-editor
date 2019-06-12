7c88c5a9a3478ab9b7a746283de8c3e7cc51e480
msrocka <michael.srocka@gmail.com>
2019-06-12 11:11:26 +0200

    probably it should be a tree
    but first we should add the dialog and create the model...

--- src/app/editors/epd/contents/ContentTable.java
+++ src/app/editors/epd/contents/ContentTable.java
@@ -1,16 +1,21 @@
 package app.editors.epd.contents;
 
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
+import app.M;
+import app.util.Actions;
 import app.util.Tables;
 import app.util.UI;
+import epd.util.Fn;
 
 class ContentTable {
 
 	boolean forPackaging;
+	private TableViewer table;
 
 	void render(FormToolkit tk, Composite body) {
 		String title = forPackaging
@@ -20,8 +25,12 @@ class ContentTable {
 		UI.gridData(section, true, true);
 		Composite comp = UI.sectionClient(section, tk);
 		UI.gridLayout(comp, 1);
+		createTable(comp);
+		createMenu();
+	}
 
-		TableViewer table = Tables.createViewer(comp,
+	private void createTable(Composite comp) {
+		table = Tables.createViewer(comp,
 				"Component/material",
 				"weight-%",
 				"kg",
@@ -40,7 +49,40 @@ class ContentTable {
 				"Material recyclable content");
 		double w = 0.8 / 9;
 		Tables.bindColumnWidths(table, 0.2, w, w, w, w, w, w, w, w, w);
+	}
 
+	private void createMenu() {
+		MenuManager menu = new MenuManager();
+		MenuManager addMenu = new MenuManager(M.Add);
+		menu.add(addMenu);
+		if (!forPackaging) {
+			addMenu.add(Actions.create("Component",
+					() -> onAdd(ContentType.COMPONENT)));
+		}
+		addMenu.add(Actions.create("Material",
+				() -> onAdd(ContentType.MATERIAL)));
+		addMenu.add(Actions.create("Substance",
+				() -> onAdd(ContentType.SUBSTANCE)));
+		menu.add(Actions.create("Edit", this::onEdit));
+		menu.add(Actions.create(M.Delete, this::onDelete));
+		Fn.with(table.getTable(),
+				table -> table.setMenu(menu.createContextMenu(table)));
+
+	}
+
+	private void onAdd(ContentType type) {
+		if (type == null)
+			return;
+		// create an instance and open the dialog
+		// if possible set the parent from the selection.
+	}
+
+	private void onEdit() {
+		// TODO
+	}
+
+	private void onDelete() {
+		// TODO
 	}
 
 }
---
+++ src/app/editors/epd/contents/ContentType.java
@@ -0,0 +1,11 @@
+package app.editors.epd.contents;
+
+enum ContentType {
+
+	COMPONENT,
+
+	MATERIAL,
+
+	SUBSTANCE,
+
+}

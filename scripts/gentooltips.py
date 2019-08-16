e8587762d1fb4cb95da1af6f5ee6a5e25ba2154d
msrocka <michael.srocka@gmail.com>
2019-08-16 16:10:45 +0200

    generate tooltips

---
+++ scripts/gentooltips.py
@@ -0,0 +1,72 @@
+
+tooltips = [
+    ("EPD", "General information"),
+    ("EPD", "UUID"),
+    ("EPD", "Name"),
+    ("EPD", "Further properties"),
+    ("EPD", "Synonyms"),
+    ("EPD", "Comment"),
+    ("EPD", "File"),
+    ("EPD", "Classification"),
+    ("EPD", "Declared product"),
+    ("EPD", "Product amount"),
+    ("EPD", "Product unit"),
+    ("EPD", "External documentation sources"),
+    ("EPD", "Uncertainty margins"),
+    ("EPD", "Uncertainty margins description"),
+    ("EPD", "Reference year"),
+    ("EPD", "Valid until"),
+    ("EPD", "Time description"),
+    ("EPD", "Location"),
+    ("EPD", "Geography description"),
+    ("EPD", "Technology description"),
+    ("EPD", "Technical prupose"),
+    ("EPD", "Pictogram"),
+    ("EPD", "Flow diagrams or pictures"),
+    ("EPD", "Modelling and validation"),
+    ("EPD", "Subtype"),
+    ("EPD", "Use advice"),
+    ("EPD", "LCA method details"),
+    ("EPD", "Data sources"),
+    ("EPD", "Compliance declarations"),
+    ("EPD", "Review"),
+    ("EPD", "Review type"),
+    ("EPD", "Review details"),
+    ("EPD", "Review report"),
+    ("EPD", "Reviewer"),
+    ("EPD", "Data entry"),
+    ("EPD", "Last update"),
+    ("EPD", "Documentor"),
+    ("EPD", "Data formats"),
+    ("EPD", "Publication and ownership"),
+    ("EPD", "Data set version"),
+    ("EPD", "Owner"),
+    ("EPD", "Copyright"),
+    ("EPD", "Access restrictions"),
+    ("EPD", "EPD profile"),
+    ("EPD", "Scenarios"),
+    ("EPD", "Modules"),
+    ("EPD", "Results"),
+]
+
+
+def make_key(tooltip):
+    k = tooltip[0] + "_"
+    label = tooltip[1]
+    for part in label.split(" "):  # type: str
+        p = part.strip()
+        k += p[0].upper() + p[1:]
+    return k
+
+
+if __name__ == "__main__":
+
+    print("Java fields:\n\n")
+    for pair in tooltips:
+        k = make_key(pair)
+        print("\tpublic static String %s;" % k)
+
+    print("\n\nMessages:\n\n")
+    for pair in tooltips:
+        k = make_key(pair)
+        print("%s = %s" % (k, pair[1]))

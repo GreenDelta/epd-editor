eacf4b6787c3d6191dc744c287f342231c08bc3e
msrocka <michael.srocka@gmail.com>
2019-08-16 16:10:27 +0200

    generate tooltips

---
+++ src/app/Tooltips.java
@@ -0,0 +1,62 @@
+package app;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Tooltips extends NLS {
+
+	public static String EPD_GeneralInformation;
+	public static String EPD_UUID;
+	public static String EPD_Name;
+	public static String EPD_FurtherProperties;
+	public static String EPD_Synonyms;
+	public static String EPD_Comment;
+	public static String EPD_File;
+	public static String EPD_Classification;
+	public static String EPD_DeclaredProduct;
+	public static String EPD_ProductAmount;
+	public static String EPD_ProductUnit;
+	public static String EPD_ExternalDocumentationSources;
+	public static String EPD_UncertaintyMargins;
+	public static String EPD_UncertaintyMarginsDescription;
+	public static String EPD_ReferenceYear;
+	public static String EPD_ValidUntil;
+	public static String EPD_TimeDescription;
+	public static String EPD_Location;
+	public static String EPD_GeographyDescription;
+	public static String EPD_TechnologyDescription;
+	public static String EPD_TechnicalPrupose;
+	public static String EPD_Pictogram;
+	public static String EPD_FlowDiagramsOrPictures;
+	public static String EPD_ModellingAndValidation;
+	public static String EPD_Subtype;
+	public static String EPD_UseAdvice;
+	public static String EPD_LCAMethodDetails;
+	public static String EPD_DataSources;
+	public static String EPD_ComplianceDeclarations;
+	public static String EPD_Review;
+	public static String EPD_ReviewType;
+	public static String EPD_ReviewDetails;
+	public static String EPD_ReviewReport;
+	public static String EPD_Reviewer;
+	public static String EPD_DataEntry;
+	public static String EPD_LastUpdate;
+	public static String EPD_Documentor;
+	public static String EPD_DataFormats;
+	public static String EPD_PublicationAndOwnership;
+	public static String EPD_DataSetVersion;
+	public static String EPD_Owner;
+	public static String EPD_Copyright;
+	public static String EPD_AccessRestrictions;
+	public static String EPD_EPDProfile;
+	public static String EPD_Scenarios;
+	public static String EPD_Modules;
+	public static String EPD_Results;
+
+	static {
+		NLS.initializeMessages("app.tooltips", M.class);
+	}
+
+	private Tooltips() {
+	}
+
+}

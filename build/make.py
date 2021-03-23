e23a747edaa89d56ac3243cadaad2cd5224364b2
msrocka <michael.srocka@gmail.com>
2021-03-23 08:27:15 +0100

    the resulting archive structure of the `shutil.make_archive`
    function is really strange when you pass in `gztar` as format parameter
    
    ```py
    import os
    import shutil
    
    if __name__ == '__main__':
        shutil.rmtree('dist')
        os.mkdir('dist')
        shutil.make_archive('dist/out', 'zip', root_dir='.', base_dir='test')
        shutil.make_archive('dist/out', 'gztar', root_dir='.', base_dir='test')
    ```
    
    so we check if the `7za` executable is available and use this by default. also,
    this should keep the file attributes...

--- build/make.py
+++ build/make.py
@@ -2,15 +2,17 @@
 import datetime
 import glob
 import os
-import os.path
 import shutil
+import subprocess
 import sys
+
+from os.path import exists
 
 
 def main():
 
     # delete the `dist` folder
-    if os.path.exists('dist'):
+    if exists('dist'):
         print('clear `dist` folder')
         shutil.rmtree('dist')
     os.mkdir('dist')
@@ -23,7 +25,7 @@ def main():
 
 def pack_win_app():
     app_dir = 'win32.win32.x86_64/epd-editor'
-    if not os.path.exists(app_dir):
+    if not exists(app_dir):
         print('%s does not exist; skip Windows version' % app_dir)
         return
 
@@ -31,17 +33,17 @@ def pack_win_app():
 
     # copy the JRE into the app
     jre_dir = 'jre/win64'
-    if not os.path.exists(jre_dir):
+    if not exists(jre_dir):
         sys.exit('ERROR: JRE not found in %s' % jre_dir)
     app_jre_dir = app_dir + '/jre'
-    if not os.path.exists(app_jre_dir):
+    if not exists(app_jre_dir):
         print('  .. copy the JRE')
         shutil.copytree(jre_dir, app_jre_dir)
 
     # create the distribution package
     version = read_app_version()
     print('  .. create package')
-    zip_file = 'dist/openLCA_win64_%s_%s' % (version, date())
+    zip_file = 'dist/epd-editor_win64_%s_%s' % (version, date())
     shutil.make_archive(zip_file, 'zip', 'win32.win32.x86_64')
     print('  done')
 
@@ -49,7 +51,7 @@ def pack_win_app():
 def pack_mac_app():
     base_dir = 'macosx.cocoa.x86_64/epd-editor'
     app_dir = 'macosx.cocoa.x86_64/epd-editor/epd-editor.app'
-    if not os.path.exists(base_dir):
+    if not exists(base_dir):
         print('%s does not exist; skip macOS version' % base_dir)
         return
 
@@ -59,8 +61,9 @@ def pack_mac_app():
     jre_tar = glob.glob('jre/*mac*.tar')
     if len(jre_tar) == 0:
         sys.exit('ERROR: could not find JRE for macOS')
-    if not os.path.exists(app_dir + '/jre'):
+    if not exists(app_dir + '/jre'):
         print('  .. copy the JRE')
+        unzip(jre_tar[0], app_dir)
         # shutil.unpack_archive(jre_tar[0], app_dir + '/epd-editor.app')
         jre_dir = glob.glob(app_dir + '/*jre*')
         for _dir in jre_dir:
@@ -76,12 +79,12 @@ def pack_mac_app():
         '/.eclipseproduct',
     ]
     for f in eclipse_contents:
-        if os.path.exists(base_dir + f):
+        if exists(base_dir + f):
             shutil.move(base_dir + f, app_dir + '/Contents/Eclipse')
 
-    if os.path.exists(base_dir + '/Resources'):
+    if exists(base_dir + '/Resources'):
         shutil.move(base_dir + '/Resources', app_dir + '/Contents')
-    if os.path.exists(base_dir+'/MacOS/epd-editor'):
+    if exists(base_dir+'/MacOS/epd-editor'):
         shutil.copy2(base_dir+'/MacOS/epd-editor',
                      app_dir + '/Contents/MacOS/eclipse')
 
@@ -99,19 +102,20 @@ def pack_mac_app():
         with open(out_ini_path, mode='w', encoding='utf-8', newline='\n') as o:
             o.write(text)
 
-    # shutil.rmtree(base_dir + "/MacOS")
-    # os.remove(base_dir + "/Info.plist")
-    # os.remove(app_dir + "/Contents/MacOS/epd-editor.ini")
+    shutil.rmtree(base_dir + "/MacOS")
+    os.remove(base_dir + "/Info.plist")
+    os.remove(app_dir + "/Contents/MacOS/epd-editor.ini")
 
     version = read_app_version()
     print('  .. create package')
-    dist_file = 'dist/openLCA_macOS_%s_%s' % (version, date())
-    shutil.make_archive(dist_file, 'gztar', 'macosx.cocoa.x86_64/epd-editor')
+    dist_file = 'dist/epd-editor_macOS_%s_%s' % (version, date())
+    # shutil.make_archive(dist_file, 'gztar', 'macosx.cocoa.x86_64/epd-editor')
+    targz('macosx.cocoa.x86_64/epd-editor', dist_file)
     print('  done')
 
 
 def read_app_version() -> str:
-    '''Read the version from the application manifest.'''
+    """Read the version from the application manifest."""
     manifest = '../META-INF/MANIFEST.MF'
     with open(manifest, 'r', encoding='utf-8') as stream:
         for line in stream:
@@ -122,6 +126,27 @@ def read_app_version() -> str:
 def date() -> str:
     now = datetime.datetime.now()
     return '%d-%02d-%02d' % (now.year, now.month, now.day)
+
+
+def targz(folder, tar_file):
+    if not exists('7za.exe'):
+        shutil.make_archive(tar_file, 'gztar', folder)
+        return
+    cmd = ['7za.exe', 'a', '-ttar', tar_file + '.tar', folder + '/*']
+    subprocess.call(cmd)
+    cmd = ['7za.exe', 'a', '-tgzip', tar_file + '.tar.gz', tar_file + '.tar']
+    subprocess.call(cmd)
+    os.remove(tar_file + '.tar')
+
+
+def unzip(zip_file, to_dir):
+    if not os.path.exists(to_dir):
+        os.makedirs(to_dir)
+    if not exists('7za.exe'):
+        shutil.unpack_archive(zip_file, to_dir)
+        return
+    cmd = ['7za.exe', 'x', zip_file, '-o%s' % to_dir]
+    subprocess.call(cmd)
 
 
 if __name__ == '__main__':

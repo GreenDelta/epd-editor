8ae8dee0a88b1eb1cf9e77fa34769d568752fad8
msrocka <michael.srocka@gmail.com>
2024-03-15 11:27:32 +0100

    draft for a Refs utility to control
    when what references should be collected from a dataset

---
+++ src/epd/Refs.java
@@ -0,0 +1,25 @@
+package epd;
+
+import org.openlca.ilcd.commons.Ref;
+import org.openlca.ilcd.flows.Flow;
+import org.openlca.ilcd.util.Processes;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class Refs {
+
+	private Refs() {
+	}
+
+	public static List<Ref> getAllEditable(Processes epd) {
+		// TODO:
+		return Collections.emptyList();
+	}
+
+	public static List<Ref> getAllEditable(Flow product) {
+		// TODO:
+		return Collections.emptyList();
+	}
+
+}

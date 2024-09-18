7675be7d469c37ab0f3ef1430c7110e2f7286160
okworx <okworx@users.noreply.github.com>
2024-09-18 14:03:04 +0200

    add new material property weight per piece

--- src/app/editors/flow/PropertyDepsDialog.java
+++ src/app/editors/flow/PropertyDepsDialog.java
@@ -168,6 +168,7 @@ class PropertyDepsDialog extends FormDialog {
 				{"volume", "gross density"},
 				{"normal volume", "bulk density"},
 				{"normal volume", "gross density"},
+                                {"number of pieces", "weight per piece"},
 			};
 
 			var selection = new HashSet<MaterialProperty>();

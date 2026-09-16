c9762c238583d893d5f3e8798219d5463c8093a4
msrocka <michael.srocka@gmail.com>
2026-09-16 15:29:20 +0200

    dependency page: do not show errors for reference data
    do not show references to indicators and units that are
    defined in the EPD profiles as errors

--- src/app/editors/DependencyPage.java
+++ src/app/editors/DependencyPage.java
@@ -1,6 +1,8 @@
 package app.editors;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.jface.viewers.ITableColorProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
@@ -9,13 +11,12 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.openlca.commons.Strings;
 import org.openlca.ilcd.commons.IDataSet;
 import org.openlca.ilcd.commons.Ref;
+import org.openlca.ilcd.epd.EpdProfiles;
 import org.openlca.ilcd.util.RefTree;
 
 import app.App;
@@ -31,6 +32,7 @@ public class DependencyPage extends FormPage {
 	private final BaseEditor editor;
 	private final IDataSet dataSet;
 	private final HashMap<String, Ref> indexRefs = new HashMap<>();
+	private final HashSet<String> profileRefs = new HashSet<>();
 
 	public DependencyPage(BaseEditor editor, IDataSet dataSet) {
 		super(editor, "DependencyPage", M.DataSetReferences);
@@ -40,10 +42,11 @@ public class DependencyPage extends FormPage {
 
 	@Override
 	protected void createFormContent(IManagedForm mform) {
-		FormToolkit tk = mform.getToolkit();
-		ScrolledForm form = UI.formHeader(mform, M.DataSetReferences);
-		Composite body = UI.formBody(form, tk);
-		TreeViewer tree = Trees.createViewer(body, M.XMLField,
+		var tk = mform.getToolkit();
+		var form = UI.formHeader(mform, M.DataSetReferences);
+		var body = UI.formBody(form, tk);
+
+		var tree = Trees.createViewer(body, M.XMLField,
 			M.DataSetReference, M.UUID, M.DataSetVersion);
 		tree.getTree().setLinesVisible(false);
 		tree.setContentProvider(new ContentProvider());
@@ -52,6 +55,7 @@ public class DependencyPage extends FormPage {
 		Trees.bindColumnWidths(tree.getTree(), 0.3, 0.5, 0.1, 0.1);
 		form.reflow(true);
 		setInput(tree);
+
 		editor.onSaved(() -> setInput(tree));
 		tree.addDoubleClickListener(_ -> {
 			RefTree.Node node = Viewers.getFirstSelected(tree);
@@ -61,16 +65,37 @@ public class DependencyPage extends FormPage {
 	}
 
 	private void setInput(TreeViewer tree) {
-		RefTree refTree = RefTree.create(dataSet);
+		var refTree = RefTree.create(dataSet);
 		indexRefs.clear();
-		for (Ref ref : refTree.getRefs()) {
+		profileRefs.clear();
+		for (var ref : refTree.getRefs()) {
 			if (!ref.isValid())
 				continue;
-			Ref r = App.index().find(ref);
+			var r = App.index().find(ref);
 			indexRefs.put(ref.getUUID(), r);
 		}
+		profileRefs.addAll(profileIds());
 		tree.setInput(refTree);
 		tree.expandAll();
+	}
+
+	/// Collects the UUIDs of the indicator and unit references that are
+	/// available in the current EPD profiles.
+	private Set<String> profileIds() {
+		var ids = new HashSet<String>();
+		for (var profile : EpdProfiles.getAll()) {
+			for (var i : profile.getIndicators()) {
+				addIdOf(ids, i.getRef());
+				addIdOf(ids, i.getUnit());
+			}
+		}
+		return ids;
+	}
+
+	private void addIdOf(Set<String> uuids, Ref ref) {
+		if (ref != null && Strings.isNotBlank(ref.getUUID())) {
+			uuids.add(ref.getUUID());
+		}
 	}
 
 	private static class ContentProvider implements ITreeContentProvider {
@@ -107,23 +132,31 @@ public class DependencyPage extends FormPage {
 
 		@Override
 		public Image getColumnImage(Object obj, int col) {
-			if (col == 2 || col == 3)
+			if (col > 1)
 				return null;
 			if (!(obj instanceof RefTree.Node node))
 				return null;
-			if (col == 0) {
-				if (node.ref == null)
-					return Icon.FOLDER.img();
-				Ref ir = indexRefs.get(node.ref.getUUID());
-				if (ir != null)
-					return Icon.OK.img();
-				else
-					return Icon.ERROR.img();
-			}
 			if (col == 1 && node.ref != null)
 				return Icon.img(node.ref.getType());
-			return null;
+
+			if (col != 0)
+				return null;
+
+			if (node.ref == null)
+				return Icon.FOLDER.img();
+			var uuid = node.ref.getUUID();
+			if (Strings.isBlank(uuid))
+				return Icon.ERROR.img();
+
+			// indicators and units of the EPD profiles are not part
+			// of the local data store
+			boolean available = indexRefs.get(uuid) != null
+				|| profileRefs.contains(uuid);
+			return available
+				? Icon.OK.img()
+				: Icon.ERROR.img();
 		}
+
 
 		@Override
 		public String getColumnText(Object obj, int col) {
--- src/app/editors/Editors.java
+++ src/app/editors/Editors.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import app.App;
-import app.AppSettings;
 import app.M;
 import app.editors.connection.ConnectionEditor;
 import app.editors.contact.ContactEditor;
@@ -129,12 +128,14 @@ public class Editors {
 	public static void addInfoPages(BaseEditor editor, IDataSet ds) {
 		if (ds == null || editor == null)
 			return;
-		AppSettings settings = App.settings();
+		var settings = App.settings();
 		try {
-			if (settings.showDataSetXML)
+			if (settings.showDataSetXML) {
 				editor.addPage(new XmlPage(editor, ds));
-			if (settings.showDataSetDependencies)
+			}
+			if (settings.showDataSetDependencies) {
 				editor.addPage(new DependencyPage(editor, ds));
+			}
 		} catch (Exception e) {
 			Logger log = LoggerFactory.getLogger(Editors.class);
 			log.error("Failed to add info pages", e);

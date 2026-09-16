5ae168eb7c3050e2473a7721d2ef66672f82f8f4
msrocka <michael.srocka@gmail.com>
2026-09-16 10:35:35 +0200

    extract the ModulesSection

---
+++ src/app/editors/epd/results/ModulesSection.java
@@ -0,0 +1,248 @@
+package app.editors.epd.results;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.openlca.ilcd.epd.EpdProfileModule;
+import org.openlca.ilcd.epd.EpdProfiles;
+import org.openlca.ilcd.processes.Process;
+import org.openlca.ilcd.processes.epd.EpdModuleEntry;
+import org.openlca.ilcd.util.Epds;
+
+import app.M;
+import app.Tooltips;
+import app.editors.epd.EpdEditor;
+import app.rcp.Icon;
+import app.util.Actions;
+import app.util.Tables;
+import app.util.UI;
+import app.util.Viewers;
+import app.util.tables.ComboModifier;
+import app.util.tables.ModifySupport;
+import app.util.tables.TextModifier;
+
+/**
+ * The section with the declared module entries of an EPD. Changes in this
+ * section are not synchronized directly with the result section; this is done
+ * via the synchronization function of the result section.
+ */
+class ModulesSection {
+
+	private final EpdEditor editor;
+	private final Process epd;
+	private final List<EpdModuleEntry> modules;
+	private final TableViewer table;
+
+	ModulesSection(EpdEditor editor, Composite body, FormToolkit tk) {
+		this.editor = editor;
+		this.epd = editor.epd;
+		this.modules = EpdModuleEntries.withAllOf(epd);
+
+		var section = UI.section(body, tk, M.Modules);
+		section.setToolTipText(Tooltips.EPD_Modules);
+		var comp = UI.sectionClient(section, tk);
+		UI.gridLayout(comp, 1);
+		var columns = new String[]{
+				M.Module,
+				M.Scenario,
+				M.Description};
+
+		table = Tables.createViewer(comp, columns);
+		table.setLabelProvider(new ModuleLabel());
+		table.getTable().setToolTipText(Tooltips.EPD_Modules);
+		Tables.addSorter(table, 0, EpdModuleEntry::getModule);
+		Tables.addSorter(table, 1, EpdModuleEntry::getScenario);
+		Tables.addSorter(table, 2, EpdModuleEntry::getDescription);
+		Tables.bindColumnWidths(table, 0.25, 0.25, 0.5);
+
+		var actions = createActions();
+		Actions.bind(section, actions);
+		Actions.bind(table, actions);
+		var modifiers = new ModifySupport<EpdModuleEntry>(table);
+		modifiers.bind(M.Module, new ModuleModifier());
+		modifiers.bind(M.Scenario, new ScenarioModifier());
+		modifiers.bind(M.Description, new DescriptionModifier());
+		table.setInput(modules);
+	}
+
+	/**
+	 * Returns the live list of the module entries of the EPD.
+	 */
+	List<EpdModuleEntry> modules() {
+		return modules;
+	}
+
+	/**
+	 * Refreshes the displayed texts of the module entries, e.g. when the
+	 * names of the scenarios have changed.
+	 */
+	void refresh() {
+		table.refresh();
+	}
+
+	private Action[] createActions() {
+		var actions = new Action[2];
+		actions[0] = Actions.create(
+				M.Add, Icon.ADD.des(), this::createEntry);
+		actions[1] = Actions.create(
+				M.Remove, Icon.DELETE.des(), this::removeEntry);
+		return actions;
+	}
+
+	private void createEntry() {
+		var e = new EpdModuleEntry().withModule(nextModule());
+		modules.add(e);
+		table.setInput(modules);
+		editor.setDirty();
+	}
+
+	private void removeEntry() {
+		EpdModuleEntry e = Viewers.getFirstSelected(table);
+		if (e == null)
+			return;
+		modules.remove(e);
+		table.setInput(modules);
+		editor.setDirty();
+	}
+
+	private String nextModule() {
+		var mods = profileModules();
+		if (mods.length == 0)
+			return null;
+		int selected = 0;
+		for (var e : modules) {
+			for (int i = 0; i < mods.length; i++) {
+				if (!Objects.equals(e.getModule(), mods[i].getName()))
+					continue;
+				if (i >= selected) {
+					selected = i + 1;
+				}
+				break;
+			}
+		}
+		if (selected < mods.length) {
+			return mods[selected].getName();
+		}
+		return mods[0].getName();
+	}
+
+	private EpdProfileModule[] profileModules() {
+		var profile = EpdProfiles.of(epd);
+		// profiles are shared instances; we copy the module list here
+		// because we should not change the order in the profile
+		var mods = new ArrayList<>(profile.getModules());
+		mods.sort(Comparator.comparingInt(EpdProfileModule::getIndex));
+		return mods.toArray(new EpdProfileModule[0]);
+	}
+
+	private static class ModuleLabel extends LabelProvider implements
+			ITableLabelProvider {
+
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		@Override
+		public String getColumnText(Object element, int col) {
+			if (!(element instanceof EpdModuleEntry e))
+				return null;
+			return switch (col) {
+				case 0 -> e.getModule();
+				case 1 -> e.getScenario();
+				case 2 -> e.getDescription();
+				default -> null;
+			};
+		}
+
+	}
+
+	private class ModuleModifier extends
+			ComboModifier<EpdModuleEntry, String> {
+
+		@Override
+		protected String getItem(EpdModuleEntry e) {
+			return e.getModule();
+		}
+
+		@Override
+		protected String[] getItems(EpdModuleEntry e) {
+			return Arrays.stream(profileModules())
+					.map(EpdProfileModule::getName)
+					.toArray(String[]::new);
+		}
+
+		@Override
+		protected String getText(String s) {
+			return s;
+		}
+
+		@Override
+		protected void setItem(EpdModuleEntry e, String module) {
+			if (Objects.equals(e.getModule(), module))
+				return;
+			e.withModule(module);
+			editor.setDirty();
+		}
+	}
+
+	private class ScenarioModifier extends
+			ComboModifier<EpdModuleEntry, String> {
+
+		@Override
+		protected String getItem(EpdModuleEntry e) {
+			return e.getScenario();
+		}
+
+		@Override
+		protected String[] getItems(EpdModuleEntry e) {
+			var scenarios = Epds.getScenarios(epd);
+			String[] names = new String[scenarios.size()];
+			for (int i = 0; i < scenarios.size(); i++) {
+				names[i] = scenarios.get(i).getName();
+			}
+			Arrays.sort(names);
+			return names;
+		}
+
+		@Override
+		protected String getText(String scenario) {
+			return scenario;
+		}
+
+		@Override
+		protected void setItem(EpdModuleEntry e, String scenario) {
+			if (Objects.equals(e.getScenario(), scenario))
+				return;
+			e.withScenario(scenario);
+			editor.setDirty();
+		}
+	}
+
+	private class DescriptionModifier extends TextModifier<EpdModuleEntry> {
+
+		@Override
+		protected String getText(EpdModuleEntry e) {
+			return e.getDescription();
+		}
+
+		@Override
+		protected void setText(EpdModuleEntry e, String text) {
+			if (Objects.equals(e.getDescription(), text))
+				return;
+			e.withDescription(text);
+			editor.setDirty();
+		}
+	}
+
+}
--- src/app/editors/epd/results/ResultPage.java
+++ src/app/editors/epd/results/ResultPage.java
@@ -1,25 +1,15 @@
 package app.editors.epd.results;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.openlca.commons.Strings;
-import org.openlca.ilcd.epd.EpdProfileModule;
 import org.openlca.ilcd.epd.EpdProfiles;
 import org.openlca.ilcd.processes.Process;
-import org.openlca.ilcd.processes.epd.EpdModuleEntry;
-import org.openlca.ilcd.util.Epds;
 
 import app.App;
 import app.M;
@@ -30,22 +20,15 @@ import app.util.Actions;
 import app.util.Controls;
 import app.util.FileChooser;
 import app.util.MsgBox;
-import app.util.Tables;
 import app.util.UI;
-import app.util.Viewers;
-import app.util.tables.ComboModifier;
-import app.util.tables.ModifySupport;
-import app.util.tables.TextModifier;
 
 public class ResultPage extends FormPage {
 
 	private final EpdEditor editor;
-
-	private final List<EpdModuleEntry> modules;
 	private final Process epd;
 	private ScenarioTable scenarioTable;
 	private ScenarioDataSection scenarioData;
-	private TableViewer moduleTable;
+	private ModulesSection modulesSection;
 	//private ResultTable resultTable;
 	private ResultMatrix resultMatrix;
 
@@ -53,7 +36,6 @@ public class ResultPage extends FormPage {
 		super(editor, "ModulesPage", M.EnvironmentalIndicators);
 		this.editor = editor;
 		epd = editor.epd;
-		modules = EpdModuleEntries.withAllOf(epd);
 	}
 
 	@Override
@@ -64,8 +46,7 @@ public class ResultPage extends FormPage {
 		createProfileSection(body, tk);
 		createScenarioSection(body, tk);
 		scenarioData = new ScenarioDataSection(editor, body, tk);
-		moduleTable = createModuleSection(body, tk);
-		moduleTable.setInput(modules);
+		modulesSection = new ModulesSection(editor, body, tk);
 		resultMatrix = createResultSection(body, tk);
 		form.reflow(true);
 	}
@@ -92,6 +73,10 @@ public class ResultPage extends FormPage {
 		Controls.onSelect(combo, _ -> {
 			int i = combo.getSelectionIndex();
 			editor.setProfile(profiles.get(i));
+			// Note: we do not refresh the module and result sections
+			// here. The declared modules do not really change with the
+			// profile; it is more the indicator IDs that are affected.
+			// We need to think about this later.
 		});
 	}
 
@@ -101,82 +86,11 @@ public class ResultPage extends FormPage {
 		section.setExpanded(false);
 		UI.gridData(section, true, false);
 		scenarioTable = new ScenarioTable(editor, section, tk);
-		scenarioTable.onChanged(() -> scenarioData.refresh());
+		scenarioTable.onChanged(() -> {
+			scenarioData.refresh();
+			modulesSection.refresh();
+		});
 		scenarioTable.setInput();
-	}
-
-	private TableViewer createModuleSection(Composite parent, FormToolkit tk) {
-		var section = UI.section(parent, tk, M.Modules);
-		section.setToolTipText(Tooltips.EPD_Modules);
-		var comp = UI.sectionClient(section, tk);
-		UI.gridLayout(comp, 1);
-		var columns = new String[]{
-				M.Module,
-				M.Scenario,
-				M.Description};
-
-		var table = Tables.createViewer(comp, columns);
-		table.setLabelProvider(new ModuleLabel());
-		table.getTable().setToolTipText(Tooltips.EPD_Modules);
-		Tables.addSorter(table, 0, EpdModuleEntry::getModule);
-		Tables.addSorter(table, 1, EpdModuleEntry::getScenario);
-		Tables.addSorter(table, 2, EpdModuleEntry::getDescription);
-		Tables.bindColumnWidths(table, 0.25, 0.25, 0.5);
-
-		var actions = createModuleActions();
-		Actions.bind(section, actions);
-		Actions.bind(table, actions);
-		var modifiers = new ModifySupport<EpdModuleEntry>(table);
-		modifiers.bind(M.Module, new ModuleModifier());
-		modifiers.bind(M.Scenario, new ScenarioModifier());
-		modifiers.bind(M.Description, new DescriptionModifier());
-		return table;
-	}
-
-	private Action[] createModuleActions() {
-		var actions = new Action[2];
-		actions[0] = Actions.create(
-				M.Add, Icon.ADD.des(), this::createModuleEntry);
-		actions[1] = Actions.create(
-				M.Remove, Icon.DELETE.des(), this::removeModule);
-		return actions;
-	}
-
-	private void createModuleEntry() {
-		var e = new EpdModuleEntry().withModule(nextModule());
-		modules.add(e);
-		moduleTable.setInput(modules);
-		editor.setDirty();
-	}
-
-	private String nextModule() {
-		var mods = modules();
-		if (mods.length == 0)
-			return null;
-		int selected = 0;
-		for (var e : modules) {
-			for (int i = 0; i < mods.length; i++) {
-				if (!Objects.equals(e.getModule(), mods[i].getName()))
-					continue;
-				if (i >= selected) {
-					selected = i + 1;
-				}
-				break;
-			}
-		}
-		if (selected < mods.length) {
-			return mods[selected].getName();
-		}
-		return mods[0].getName();
-	}
-
-	private void removeModule() {
-		EpdModuleEntry e = Viewers.getFirstSelected(moduleTable);
-		if (e == null)
-			return;
-		modules.remove(e);
-		moduleTable.setInput(modules);
-		editor.setDirty();
 	}
 
 	private ResultMatrix createResultSection(Composite body, FormToolkit tk) {
@@ -187,7 +101,7 @@ public class ResultPage extends FormPage {
 		UI.gridLayout(composite, 1);
 		Actions.bind(section, createResultActions());
 		var matrix = new ResultMatrix(editor, composite);
-		matrix.render(modules);
+		matrix.render(modulesSection.modules());
 		return matrix;
 	}
 
@@ -197,7 +111,7 @@ public class ResultPage extends FormPage {
 				Icon.CHECK_TRUE.des(), () -> {
 					new ResultSync(epd, editor.getProfile()).run();
 					// resultTable.refresh();
-					resultMatrix.render(modules);
+					resultMatrix.render(modulesSection.modules());
 					editor.setDirty();
 				});
 		actions[1] = Actions.create(
@@ -227,119 +141,12 @@ public class ResultPage extends FormPage {
 		var resultImport = ExcelImport.of(epd, editor.getProfile(), file);
 		App.run(M.Import, resultImport, () -> {
 			// resultTable.refresh();
-			moduleTable.refresh();
+			modulesSection.refresh();
 			scenarioTable.setInput();
 			scenarioData.refresh();
-			resultMatrix.render(modules);
+			resultMatrix.render(modulesSection.modules());
 			editor.setDirty();
 		});
-	}
-
-	private EpdProfileModule[] modules() {
-		var profile = EpdProfiles.of(epd);
-		var modules = profile.getModules();
-		modules.sort(Comparator.comparingInt(EpdProfileModule::getIndex));
-		return modules.toArray(new EpdProfileModule[0]);
-	}
-
-	private static class ModuleLabel extends LabelProvider implements
-			ITableLabelProvider {
-
-		@Override
-		public Image getColumnImage(Object element, int columnIndex) {
-			return null;
-		}
-
-		@Override
-		public String getColumnText(Object element, int col) {
-			if (!(element instanceof EpdModuleEntry e))
-				return null;
-			return switch (col) {
-				case 0 -> e.getModule();
-				case 1 -> e.getScenario();
-				case 2 -> e.getDescription();
-				default -> null;
-			};
-		}
-
-	}
-
-	private class ModuleModifier extends
-		ComboModifier<EpdModuleEntry, String> {
-
-		@Override
-		protected String getItem(EpdModuleEntry e) {
-			return e.getModule();
-		}
-
-		@Override
-		protected String[] getItems(EpdModuleEntry e) {
-			return Arrays.stream(modules())
-					.map(EpdProfileModule::getName)
-					.toArray(String[]::new);
-		}
-
-		@Override
-		protected String getText(String s) {
-			return s;
-		}
-
-		@Override
-		protected void setItem(EpdModuleEntry e, String module) {
-			if (Objects.equals(e.getModule(), module))
-				return;
-			e.withModule(module);
-			editor.setDirty();
-		}
-	}
-
-	private class ScenarioModifier extends
-		ComboModifier<EpdModuleEntry, String> {
-
-		@Override
-		protected String getItem(EpdModuleEntry e) {
-			return e.getScenario();
-		}
-
-		@Override
-		protected String[] getItems(EpdModuleEntry e) {
-			var scenarios = Epds.getScenarios(epd);
-			String[] names = new String[scenarios.size()];
-			for (int i = 0; i < scenarios.size(); i++) {
-				names[i] = scenarios.get(i).getName();
-			}
-			Arrays.sort(names);
-			return names;
-		}
-
-		@Override
-		protected String getText(String scenario) {
-			return scenario;
-		}
-
-		@Override
-		protected void setItem(EpdModuleEntry e, String scenario) {
-			if (Objects.equals(e.getScenario(), scenario))
-				return;
-			e.withScenario(scenario);
-			editor.setDirty();
-		}
-	}
-
-	private class DescriptionModifier extends TextModifier<EpdModuleEntry> {
-
-		@Override
-		protected String getText(EpdModuleEntry e) {
-			return e.getDescription();
-		}
-
-		@Override
-		protected void setText(EpdModuleEntry e, String text) {
-			if (Objects.equals(e.getDescription(), text))
-				return;
-			e.withDescription(text);
-			editor.setDirty();
-		}
 	}
 
 }

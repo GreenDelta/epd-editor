34c532d64363eda42283d959ab8eea57733ee1ab
msrocka <michael.srocka@gmail.com>
2026-09-22 15:34:04 +0200

    better design of the EpdProfileEditor.Page

--- src/app/editors/profiles/Page.java
+++ src/app/editors/profiles/Page.java
@@ -1,9 +1,7 @@
 package app.editors.profiles;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
@@ -14,10 +12,9 @@ import org.openlca.ilcd.epd.EpdProfile;
 import app.App;
 import app.M;
 import app.StatusView;
-import app.editors.refs.RefSelectionDialog;
+import app.editors.refs.RefLink;
 import app.navi.NaviSync;
 import app.rcp.Texts;
-import app.store.Profiles;
 import app.store.RefDataSync;
 import app.util.Controls;
 import app.util.MsgBox;
@@ -48,104 +45,57 @@ class Page extends FormPage {
 	}
 
 	private void infoSection(FormToolkit tk, Composite body) {
-		Composite comp = UI.formSection(body, tk, M.GeneralInformation);
-		UI.gridLayout(comp, 3);
+		var comp = UI.formSection(body, tk, M.GeneralInformation);
 
 		// built-in profiles cannot be modified
 		if (readOnly) {
 			var notice = UI.formLabel(comp, tk, M.BuiltInProfileNotice);
 			UI.applyItalicFont(notice);
-			if (notice.getLayoutData() instanceof GridData gd) {
-				gd.horizontalSpan = 3;
-			}
-			UI.filler(comp);
-			var duplicate = tk.createButton(comp, M.DuplicateProfile, SWT.NONE);
-			Controls.onSelect(duplicate, _ -> duplicate());
-			UI.filler(comp);
+			UI.stretchNone(notice).horizontalSpan = 2;
 		}
 
 		// name
-		Text nameText = UI.formText(comp, tk, M.Name);
+		var nameText = UI.formText(comp, tk, M.Name);
 		Texts.set(nameText, profile.getName()).setEditable(!readOnly);
-		if (readOnly) {
-			UI.filler(comp);
-		} else {
+		if (!readOnly) {
 			nameText.addModifyListener(_ -> {
 				profile.withName(nameText.getText());
 				editor.setDirty();
 			});
-			UI.filler(comp);
 		}
 
 		// description
-		Text descrText = UI.formMultiText(comp, tk, M.Description);
+		var descrText = UI.formMultiText(comp, tk, M.Description);
 		Texts.set(descrText, profile.getDescription()).setEditable(!readOnly);
-		if (readOnly) {
-			UI.filler(comp);
-		} else {
+		if (!readOnly) {
 			descrText.addModifyListener(_ -> {
 				profile.withDescription(descrText.getText());
 				editor.setDirty();
 			});
-			UI.filler(comp);
+		}
+
+		UI.formLabel(comp, tk, M.ComplianceSystem);
+		var link = new RefLink(comp, tk, DataSetType.SOURCE);
+		link.setRef(profile.getComplianceSystem());
+		if (!readOnly) {
+			link.onChange(ref -> {
+				profile.withComplianceSystem(ref);
+				editor.setDirty();
+			});
 		}
 
 		// reference data URL
-		Text urlText = UI.formText(comp, tk, M.ReferenceDataURL);
+		var urlText = UI.formText(comp, tk, M.ReferenceDataURL);
 		Texts.set(urlText, profile.getDataUrl()).setEditable(!readOnly);
-		if (readOnly) {
-			UI.filler(comp);
-		} else {
+		if (!readOnly) {
 			urlText.addModifyListener(_ -> {
 				profile.withDataUrl(urlText.getText());
 				editor.setDirty();
 			});
-			UI.filler(comp);
 		}
-
-		complianceSystem(tk, comp);
-
-		// download reference data sets
 		UI.filler(comp);
 		var button = tk.createButton(comp, M.DownloadDataSets, SWT.NONE);
 		Controls.onSelect(button, _ -> syncRefData(urlText.getText()));
-		UI.filler(comp);
-	}
-
-	private void complianceSystem(FormToolkit tk, Composite comp) {
-		UI.formLabel(comp, tk, M.ComplianceSystem);
-		var ref = profile.getComplianceSystem();
-		var value = tk.createLabel(comp, ref != null ? App.s(ref) : M.None);
-		UI.stretchX(value);
-		if (readOnly) {
-			UI.filler(comp);
-			return;
-		}
-		var buttons = UI.formComposite(comp, tk);
-		var select = tk.createButton(buttons, M.SelectDataSet, SWT.NONE);
-		Controls.onSelect(select, _ -> {
-			var selected = RefSelectionDialog.select(DataSetType.SOURCE);
-			if (selected == null)
-				return;
-			profile.withComplianceSystem(selected);
-			value.setText(App.s(selected));
-			editor.setDirty();
-		});
-		var clear = tk.createButton(buttons, M.Remove, SWT.NONE);
-		Controls.onSelect(clear, _ -> {
-			profile.withComplianceSystem(null);
-			value.setText(M.None);
-			editor.setDirty();
-		});
-	}
-
-	private void duplicate() {
-		var copy = Profiles.copyOf(profile);
-		if (copy == null) {
-			MsgBox.error(M.FailedToSaveProfile);
-			return;
-		}
-		ProfileEditor.open(copy);
 	}
 
 	private void syncRefData(String url) {
